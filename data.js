edfb5440669f486ca6fbfd5208837a1cb0bd3b8b
Arif ORAK <ariforak91@gmail.com>
2023-03-19 18:18:39 +0300

    ShopAplication.Front with Json-Server

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = function(){
+  return {
+    products: [
+      {id:1, name:"Samsung S5",price:11000,imageUrl:'1.jpg', description:"idare eder."},
+      {id:1, name:"Samsung S6",price:12000,imageUrl:'2.jpg', description:"Çok iyi"},
+      {id:1, name:"Samsung S7",price:13000,imageUrl:'3.jpg', description:"idare eder"},
+      {id:1, name:"Samsung S8",price:15000,imageUrl:'4.jpg', description:"Önemli telefon"},
+      {id:1, name:"Samsung S9",price:19000,imageUrl:'5.jpg', description:"idare eder."},
+      {id:1, name:"Samsung S10",price:22000,imageUrl:'6.jpg', description:"idare eder."},
+      {id:1, name:"Samsung S11",price:24000,imageUrl:'7.jpg', description:"idare eder."}
+
+    ],
+    categories : [
+      {id:1,name:"Telefon"},
+      {id:2,name:"Bilgisayar"},
+      {id:1,name:"Elektronik"}
+    ],
+    orders :[
+
+    ]
+
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'root',
+  template: `<div class="bg-primary m-5 p-5">Shop Application</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopApp';
+
 }
